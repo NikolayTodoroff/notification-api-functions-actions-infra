f188c6c56a5928293992187c97b7a387649051e0
Nikolay Todorov <everflowing555@gmail.com>
2026-06-16 11:11:24 +0300

    ci: add application workflow and fix dotnet versions in app csproj files

--- infra/modules/function-app/main.tf
+++ infra/modules/function-app/main.tf
@@ -35,7 +35,7 @@ resource "azurerm_linux_function_app" "function_app" {
 
   site_config {
     application_stack {
-      dotnet_version              = "10.0"
+      dotnet_version              = "8.0"
       use_dotnet_isolated_runtime = true
     }
   }
